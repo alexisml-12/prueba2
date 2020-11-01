14478b49c35900a756e16b79c46740dfdeb515d0
Alexis <amurillo82943@misena.edu.co>
2020-11-01 13:32:14 -0500

    YEP

--- index.php
+++ index.php
@@ -3,9 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.css">
     <title>Document</title>
 </head>
 <body>
-    <h1> hola worlddddddddd </h1>
+    <div class="container">
+        <h1>Holaaaa</h1>
+        <p>fnkjdsbhfkjdsfhñoksdfndfjbskjdfbsjdbfkdsjf</p>
+        <button class="btn btn-primary">szs</button>
+    </div>
+
+
+
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
